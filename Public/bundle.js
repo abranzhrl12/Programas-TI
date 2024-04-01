8ce31203f6c1ef27b15bedd4209f09a2f0ad64c0
unknown <netxrl03@gmail.com>
2024-03-31 23:48:54 -0500

    Primera Version de la Pagina

---
+++ Public/bundle.js
@@ -0,0 +1,177 @@
+'use strict';
+
+const btnPotoshopm=document.getElementById('btnPotoshop-m');
+btnPotoshopm.addEventListener('click',()=>{
+    window.open("https://www.mediafire.com/file/nfafbfs7lyten91/Adobe_Photoshop_2024_.rar/file", "_blank");
+});
+
+const btnPotoshopd=document.getElementById('btnPotoshop-d');
+btnPotoshopd.addEventListener('click',()=>{
+    window.open("https://drive.google.com/file/d/1BfwR1Kw3R9GSofYj0J2cEGqCArOdO1HC/view?usp=sharing", "_blank");
+});
+
+//adobe XD
+//-Mediafire
+const adobexdm=document.getElementById('btnXD-m');
+adobexdm.addEventListener('click',()=>{
+    window.open("https://www.mediafire.com/file/wpkubpbpf1fydgx/Adobe_XD_2023.rar/file", "_blank");
+});
+
+//-Drive
+const adobexdd=document.getElementById('btnXD-d');
+adobexdd.addEventListener('click',()=>{
+    window.open("https://drive.google.com/file/d/1d3tCNv_fQd0jzoMECffDtAbdIdhise6v/view?usp=sharing", "_blank");
+});
+
+
+
+const ilustratorm=document.getElementById('btnilustrator-m');
+ilustratorm.addEventListener('click',()=>{
+    window.open("https://www.mediafire.com/file/acy7ogmi657akqo/Adobe_Illustrator_2024_%2528Pre_Activado%2529.rar/file", "_blank");
+});
+
+
+const ilustratord=document.getElementById('btnilustrator-d');
+ilustratord.addEventListener('click',()=>{
+    window.open("https://drive.google.com/file/d/1yWKOEptjrg1kRvuefFfdY0w5g6dRH0jn/view?usp=sharing", "_blank");
+});
+
+// corel draw
+const coreldrawm=document.getElementById('coreldraw-m');
+coreldrawm.addEventListener('click',()=>{
+    window.open("https://www.mediafire.com/file/15i3wc1haia8qsb/CorelDRAW__2024.rar/file", "_blank");
+});
+
+const coreldrawd=document.getElementById('coreldraw-d');
+coreldrawd.addEventListener('click',()=>{
+    window.open("https://drive.google.com/file/d/1DCvSMVgLJEZSk6qBUwfboVTlGJY7kyjh/view?usp=sharing", "_blank");
+});
+
+const acrobatm=document.getElementById('acrobat-m');
+acrobatm.addEventListener('click',()=>{
+    window.open("https://www.mediafire.com/file/23cryuoqgo43t27/Adobe_Acrobat_Pro_DC_2024.rar/file", "_blank");
+});
+
+const acrobatd=document.getElementById('acrobat-d');
+acrobatd.addEventListener('click',()=>{
+    window.open("https://drive.google.com/file/d/1U9Eaz8MZUA1xkc1fpk9XxrL0JJy1Q9Mz/view?usp=sharing", "_blank");
+});
+// nitro
+const nitrom=document.getElementById('nitro-m');
+nitrom.addEventListener('click',()=>{
+    window.open("https://www.mediafire.com/file/u9sgl87f2mh3u25/nitro_Enterprise_2024.rar/file", "_blank");
+});
+
+const nitrod=document.getElementById('nitro-d');
+nitrod.addEventListener('click',()=>{
+    window.open("https://drive.google.com/file/d/14GFZakMLk1lhkzYyy-GjgQ-b1I4EjL6k/view?usp=sharing", "_blank");
+});
+// driver boster
+const bosterm=document.getElementById('boster-m');
+bosterm.addEventListener('click',()=>{
+    window.open("https://www.mediafire.com/file/6r55mpwmga69pjr/IObit_Driver_Booster_Pro.v11.3.rar/file", "_blank");
+});
+
+const bosterd=document.getElementById('boster-d');
+bosterd.addEventListener('click',()=>{
+    window.open("https://drive.google.com/file/d/1qA7uUUUahMc1DnntJT4uWsesPkNUr_mL/view?usp=sharing", "_blank");
+});
+// office
+const officem=document.getElementById('office-m');
+officem.addEventListener('click',()=>{
+    window.open("https://www.mediafire.com/file/ap7a8gslzjsu0q5/Office_2016-2024_online.rar/file", "_blank");
+});
+
+const officed=document.getElementById('office-d');
+officed.addEventListener('click',()=>{
+    window.open("https://drive.google.com/file/d/1MBW_ov5OhdpYdgSkm7-2BgLpXSnEyTyd/view?usp=sharing", "_blank");
+});
+
+// filmora
+
+const filmoram=document.getElementById('filmora-m');
+filmoram.addEventListener('click',()=>{
+    window.open("https://www.mediafire.com/file/cwhd7b4h48c30e4/FIlmora_13.rar/file", "_blank");
+});
+
+const filmorad=document.getElementById('filmora-d');
+filmorad.addEventListener('click',()=>{
+    window.open("https://drive.google.com/file/d/1_sUVWk2ns-iSSQBC8TF3Xo0tM2ch76CA/view?usp=sharing", "_blank");
+});
+
+// topaz photo ia
+
+const topazpm=document.getElementById('topazp-m');
+topazpm.addEventListener('click',()=>{
+    window.open("https://www.mediafire.com/file/lyxyydy42q5w8xi/Topaz_Photo_AI.v2.4.1.rar/file", "_blank");
+});
+
+
+const topazpd=document.getElementById('topazp-d');
+topazpd.addEventListener('click',()=>{
+    window.open("https://drive.google.com/file/d/1dJN0BF20lne-4npgrgQnvGmoVl3yNLv2/view?usp=sharing", "_blank");
+});
+
+// activador windows 11
+
+const win11m=document.getElementById('win11-m');
+win11m.addEventListener('click',()=>{
+    window.open("https://www.mediafire.com/file/da6sza1t5drrxvn/Activador_windows_11.rar/file", "_blank");
+});
+
+const win11d=document.getElementById('win11-d');
+win11d.addEventListener('click',()=>{
+    window.open("https://drive.google.com/file/d/1wcWWoV43YDSOKFWddOjzNaQGvGFJLa3P/view?usp=sharing", "_blank");
+});
+
+// vcl
+
+const vclm=document.getElementById('vcl-m');
+vclm.addEventListener('click',()=>{
+    window.open("https://www.mediafire.com/file/3z6ma9rjd7z0hx6/vlc-3.0.20-win64.rar/file", "_blank");
+});
+
+const vcld=document.getElementById('vlc-d');
+vcld.addEventListener('click',()=>{
+    window.open("https://drive.google.com/file/d/1RCjY2ht9M1e_Z3Pab1nrpDXnDtD7tX25/view?usp=sharing", "_blank");
+});
+// google earth
+
+const googleearthm=document.getElementById('googleearth-m');
+googleearthm.addEventListener('click',()=>{
+    window.open("https://www.mediafire.com/file/tii4r08y2achiu5/GoogleEarthProWin-x64.rar/file", "_blank");
+});
+
+const googleearthd=document.getElementById('googleearth-d');
+googleearthd.addEventListener('click',()=>{
+    window.open("https://drive.google.com/file/d/1sGTXNYzT9TvUNDnS4GTAkScWSQdsH68D/view?usp=sharing", "_blank");
+});
+
+// adobe premier
+
+
+const premierm=document.getElementById('premier-m');
+premierm.addEventListener('click',()=>{
+    window.open("https://www.mediafire.com/file/f6yujfflwyce5eu/Adobe_Premiere_Pro_2024_PreActivado.X64.rar/file", "_blank");
+});
+
+
+
+const premierd=document.getElementById('premier-d');
+premierd.addEventListener('click',()=>{
+    window.open("https://drive.google.com/file/d/1WJG_oACCISvK9ktl3DROZJC1u778uW0y/view?usp=sharing", "_blank");
+});
+
+// IObit Unistaller
+
+const IobitUnistallerm=document.getElementById('IobitUnistaller-m');
+IobitUnistallerm.addEventListener('click',()=>{
+    window.open("https://www.mediafire.com/file/bsq5q29d31oxm5u/Iobit_Unistaller_Pro_13.rar/file", "_blank");
+});
+
+const IobitUnistallerd=document.getElementById('IobitUnistaller-d');
+IobitUnistallerd.addEventListener('click',()=>{
+    window.open("https://drive.google.com/file/d/1SFbqV9wTVu0ve5Lm7eMfyG-thR2odHq3/view?usp=sharing", "_blank");
+});
+
+
